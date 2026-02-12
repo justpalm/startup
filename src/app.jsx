a56849c7ca50253d92cee6fc506fdeec89501768
justpalm <palmerjustins@gmail.com>
2026-02-11 23:15:04 -0700

    Got the app.jsx with the header and footer elements

--- src/app.jsx
+++ src/app.jsx
@@ -3,5 +3,21 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import './app.css';
 
 export default function App() {
-  return <div className="body bg-dark text-light">App will display here</div>;
+  return (
+    <div className="body bg-dark text-light">
+      <header>
+      <h1>The Minotaur<sup>&reg;</sup></h1>
+    </header>
+
+      <main>App components go here</main>
+
+    <footer>
+      <hr />
+      <span class="text-reset">Author Name:</span>
+      <span class="text-reset">Justin Palmer </span>
+      <br />
+      <a href="https://github.com/justpalm/startup.git">GitHub</a>
+    </footer>
+    </div>
+  );
 }
