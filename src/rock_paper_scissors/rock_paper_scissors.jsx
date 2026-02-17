f35554cba0326c1dae4880774a837115b5287f9b
justpalm <palmerjustins@gmail.com>
2026-02-16 21:56:32 -0700

    Rock Paper Scissors works!

--- src/rock_paper_scissors/rock_paper_scissors.jsx
+++ src/rock_paper_scissors/rock_paper_scissors.jsx
@@ -2,19 +2,20 @@ import React, { useState } from "react";
 import Button from "react-bootstrap/Button";
 import "bootstrap/dist/css/bootstrap.min.css";
 import { useNavigate } from "react-router-dom";
-import c1 from ".. character/characters/character_1.png";
-import c2 from ".. character/characters/character_2.png";
-import c3 from ".. character/characters/character_3.png";
+import c1 from "../character/characters/character_1.png";
+import c2 from "../character/characters/character_2.png";
+import c3 from "../character/characters/character_3.png";
 
 export function Rock_Paper_Scissors() {
   return (
     <main>
       <h4>World: Inert messages that comes from the websocket or game</h4>
       
+      <h1>Rock Paper Scissors</h1>
       <br />
       <input type="text" id="count" value="Time - 4:32" readOnly />  {}
       
-      <h1>Rock Paper Scissors</h1>
+      
       
       <img 
         id="Minotaur" 
@@ -31,12 +32,41 @@ export function Rock_Paper_Scissors() {
         alt="Character" /> 
 
       {/* Fight area */}
-      <Button onClick={() => {}}>✂️</Button>
-      <Button onClick={() => {}}>🪨</Button>
-      <Button onClick={() => {}}>📄</Button>
+      <Button 
+      variant= "light"
+      onClick={() => {}}>✂️</Button>
+      <Button 
+      variant = "light"
+      onClick={() => {}}>🪨</Button>
+      <Button 
+      variant = "light"
+      onClick={() => {}}>📄</Button>
 
-      <Button variant="button" onClick={() => navigate("/_vistory_")}Victory</Button>
-      <Button variant="button" onClick={() => navigate("/maze")}>Defeat</Button>
+      <Button 
+      variant="success"
+      onClick={() => navigate("/victory")}
+      >
+      Victory
+      </Button>
+
+      <Button 
+      variant="danger" 
+      onClick={() => navigate("/maze")}
+      >
+      Defeat
+      </Button>
+
+
+      <Button
+          variant="outline-warning"
+          onClick={() => navigate("/")}
+          size = "lg">
+          Go Back
+      </Button>
+
+
+
+
 
     </main>
   );
